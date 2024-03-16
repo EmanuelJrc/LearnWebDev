9d9e4a458987879cdf0232c098c16c4ace4d9f43
Emanuel_jrc <emanueljuricev5@gmail.com>
2024-03-16 21:34:58 +0100

    Added courses and contact page

--- script.js
+++ script.js
@@ -44,3 +44,10 @@ var swiper = new Swiper(".teachers-slider", {
     },
   },
 });
+
+document.querySelector(".load-more .btn").onclick = () => {
+  document.querySelectorAll(".courses .box-container .hide").forEach((show) => {
+    show.style.display = "block";
+  });
+  document.querySelector(".load-more .btn").style.display = "none";
+};
